a4b02016a5fa331e796a2ddf7ac10aa9c94d5835
Carly Paige <carly.paige@gmail.com>
2022-09-29 11:15:06 -0500

    Integrated API into elements

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Meaning(props) {
-  return "hello from meaning";
+  return (
+    <div className="Meaning">
+      <h3>
+        {"("}
+        {props.meaning.partOfSpeech}
+        {")"}
+      </h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {props.meaning.definitions[0].definition}
+              <br />
+              {props.meaning.definitions[0].example}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
