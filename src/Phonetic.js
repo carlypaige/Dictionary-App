805f9569a77cd96fe782c5808f915687ceb0dcba
Carly Paige <carly.paige@gmail.com>
2022-10-03 13:16:36 -0500

    fixed rels

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        rel="noopener"
-      >
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
