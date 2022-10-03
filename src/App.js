9d7563176f93a5c61d8535f12b60f2e3c6f56ab4
Carly Paige <carly.paige@gmail.com>
2022-10-03 13:33:40 -0500

    Styled footer tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
         <h1 className="App-header">Dictionary</h1>
         <Dictionary defaultKeyword="Hi" />
         <a
-          className="footer pb-5"
+          className="footer pb-4 mt-1 text-decoration-none"
           href="https://github.com/carlypaige/dictionary-app"
           rel="noopener noreferrer"
         >
